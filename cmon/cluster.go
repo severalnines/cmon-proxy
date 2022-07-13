c6ebeb9dcdc5507a04748eaa24ef96a8cafad938
David Kedves <kedz@kedz.eu>
2022-07-13 09:26:49 +0200

    fix for crash when not a full req structure is passed to cluster APIs

--- cmon/cluster.go
+++ cmon/cluster.go
@@ -7,6 +7,9 @@ import (
 )
 
 func (client *Client) GetClusterInfo(req *api.GetClusterInfoRequest) (*api.GetClusterInfoResponse, error) {
+	if req.WithOperation == nil {
+		req.WithOperation = &api.WithOperation{}
+	}
 	req.Operation = "getClusterInfo"
 	if req.WithClusterID == nil || req.WithClusterID.ClusterID < 1 {
 		return nil, fmt.Errorf("invalid cluster id")
@@ -25,6 +28,9 @@ func (client *Client) GetAllClusterInfo(req *api.GetAllClusterInfoRequest) (*api
 	if req == nil {
 		req = &api.GetAllClusterInfoRequest{}
 	}
+	if req.WithOperation == nil {
+		req.WithOperation = &api.WithOperation{}
+	}
 	req.Operation = "getAllClusterInfo"
 	res := &api.GetAllClusterInfoResponse{}
 	if err := client.Request(api.ModuleClusters, req, res); err != nil {
@@ -37,6 +43,9 @@ func (client *Client) GetAllClusterInfo(req *api.GetAllClusterInfoRequest) (*api
 }
 
 func (client *Client) CreateDatabase(req *api.CreateDatabaseRequest) (*api.CreateDatabaseResponse, error) {
+	if req.WithOperation == nil {
+		req.WithOperation = &api.WithOperation{}
+	}
 	req.Operation = "createDatabase"
 	if req.WithClusterID == nil || req.WithClusterID.ClusterID < 1 {
 		return nil, fmt.Errorf("invalid cluster id")
---
+++ simpletestclient/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/severalnines/cmon-proxy/cmon"
+	"github.com/severalnines/cmon-proxy/config"
+	"github.com/severalnines/cmon-proxy/logger"
+	"go.uber.org/zap"
+)
+
+// entry point. no logic here.
+func main() {
+	logger.New(logger.DefaultConfig())
+	zap.L().Info("Tester...")
+
+	client := cmon.NewClient(&config.CmonInstance{Url: "https://127.0.0.1:9501", Username: "kedz", Password: "password"}, 30)
+	err := client.Authenticate()
+	if err != nil {
+		log.Println("%+v", err)
+		res, err := client.Ping()
+		log.Println("%+v  %+v", err, res)
+		return
+	}
+	//      req := api.GetAllClusterInfoRequest{}
+	//      req.Operation = "getAllClusterInfo"
+	res, err := client.GetAllClusterInfo(nil)
+	log.Println("%+v", res)
+}
