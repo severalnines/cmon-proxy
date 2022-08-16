a822fe68fc8b936845410360d590b36d994c10a3
Johan Andersson <johan@severalnines.com>
2022-08-16 16:08:26 +0200

    Added license info - GPL 2

--- cmon/api/auth.go
+++ cmon/api/auth.go
@@ -1,4 +1,14 @@
 package api
+// Copyright 2022 Severalnines AB
+//
+// This file is part of cmon-proxy.
+//
+// cmon-proxy is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2 of the License.
+//
+// cmon-proxy is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with cmon-proxy. If not, see <https://www.gnu.org/licenses/>.
+
 
 // AuthenticateRequest the one to star authentication (key or password based)
 type AuthenticateRequest struct {
